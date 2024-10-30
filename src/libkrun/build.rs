03959a3cb4596c4cfcfa1fb4fda0a31c36eb9362
Stephen Akinyemi <appcypher@outlook.com>
2024-10-30 15:30:14 +0100

    feat: change ld link path from /opt/hombrew/lib to /usr/local/lib on macos (#3)

--- src/libkrun/build.rs
+++ src/libkrun/build.rs
@@ -1,9 +1,15 @@
 fn main() {
-    #[cfg(target_os = "linux")]
-    println!(
-        "cargo:rustc-cdylib-link-arg=-Wl,-soname,libkrun.so.{}",
-        std::env::var("CARGO_PKG_VERSION_MAJOR").unwrap()
-    );
+    // #[cfg(target_os = "linux")]
+    // println!(
+    //     "cargo:rustc-cdylib-link-arg=-Wl,-soname,libkrun.so.{}",
+    //     std::env::var("CARGO_PKG_VERSION_MAJOR").unwrap()
+    // );
+    // #[cfg(target_os = "macos")]
+    // println!("cargo:rustc-link-lib=framework=Hypervisor");
     #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-lib=framework=Hypervisor");
+    println!("cargo:rustc-link-search=/usr/local/lib");
+    #[cfg(all(not(feature = "tee"), not(feature = "efi")))]
+    println!("cargo:rustc-link-lib=krunfw");
+    #[cfg(feature = "tee")]
+    println!("cargo:rustc-link-lib=krunfw-sev");
 }
